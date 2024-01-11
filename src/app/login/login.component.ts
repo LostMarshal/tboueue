c20b929d941dcc3d39b210ddf023f343338bce3d
Fardila Bintang <fardilabintang476@gmail.com>
2024-01-12 00:27:21 +0700

    Code Refining

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose,} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
+
 export class LoginComponent {
+
+  constructor(private router: Router) {}
+  
   account = [
     {
       username: 'user',
@@ -29,7 +37,7 @@ export class LoginComponent {
 
   // ...
 
-  constructor(private router: Router) {}
+  
 
   login() {
     const foundUser = this.account.find(
